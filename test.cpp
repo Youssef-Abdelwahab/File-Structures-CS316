151716c88d7d7cac090bf26ad81fded51aef5a01
Youssef Abdelwahab <110339904+Youssef-Abdelwahab@users.noreply.github.com>
2024-03-08 00:00:55 +0200

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	int num;
+	cout << "Enter number :";
+	cin >> num;
+
+	for (int i = 0;i < num;i++){
+        cout << i << "\n";
+	}
+	return 0;
+}
