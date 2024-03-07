1316e64ccd84a184aa15349573200cbfe9f4f471
Youssef Abdelwahab <110339904+Youssef-Abdelwahab@users.noreply.github.com>
2024-03-08 00:07:06 +0200

    lab03

---
+++ Lab_03/GradeBook.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "GradeBook.h"
+using namespace std;
+
+GradeBook::GradeBook(string name){
+    setCourseName(name);
+}
+
+void GradeBook::setCourseName(string name){
+    if (name.size() <= 25)
+    {
+        courseName = name;
+    } else {
+        courseName = name.substr(0, 25);
+        cerr << "Name \"" << name << "\" exceeds maximum length (25).\n"
+        << "Limiting courseName to first 25 characters.\n" << endl;
+    }
+}
+
+string GradeBook::getCourseName() const {
+    return courseName;
+}
+
+void GradeBook::displayMassege() const {
+    cout << "Welcome to the grade book for\n" << getCourseName() << "!" << endl;
+}
